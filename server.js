1981cdb83892de56692ba7ab345f763bb376ddd9
Big Head <spencer.moyer16@yahoo.com>
2021-04-05 22:18:43 -0700

    added parameter for animal IDs

--- server.js
+++ server.js
@@ -43,7 +43,12 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
-}
+};
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -51,6 +56,16 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    }
+    else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
